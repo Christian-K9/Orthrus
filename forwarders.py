52c6f2b50eb65ad9067a6b7bbd83f5c5ac954aef
Christian-K9 <164112138+Christian-K9@users.noreply.github.com>
2025-10-18 20:18:08 -0400

    Update forwarders.py

--- forwarders.py
+++ forwarders.py
@@ -1,25 +1,15 @@
 import subprocess
 import sys
 
-def install_packageg(package, installer):
-    try:
-        subprocess.check_call(["sudo", installer, "install", "-y", package])
-    except subprocess.CalledProcessError as e:
-        print("Error Installing: " + package)
+splunk_path = r"C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe"
+splunk_server = input("What Is the Splunk Server IP?: ")
+AUTH = "admin:", input("What Is The Admin Password?: ")
 
+subprocess.run([splunk_path, "add", "forward-server", splunk_server])
 
-machines = {"debian": 'splunkforwarder-10.0.1-c486717c322b-linux-amd64.deb "https://download.splunk.com/products/universalforwarder/releases/10.0.1/linux/splunkforwarder-10.0.1-c486717c322b-linux-amd64.deb"',
-            "ubuntu": 'splunkforwarder-10.0.1-c486717c322b-linux-amd64.deb "https://download.splunk.com/products/universalforwarder/releases/10.0.1/linux/splunkforwarder-10.0.1-c486717c322b-linux-amd64.deb"',
-            "centOS": 'splunkforwarder-10.0.1-c486717c322b-linux-amd64.deb "https://download.splunk.com/products/universalforwarder/releases/10.0.1/linux/splunkforwarder-10.0.1-c486717c322b-linux-amd64.deb"',
-            "fedora": 'splunkforwarder-10.0.1-c486717c322b.x86_64.rpm "https://download.splunk.com/products/universalforwarder/releases/10.0.1/linux/splunkforwarder-10.0.1-c486717c322b.x86_64.rpm"',
-            "windows": 'splunkforwarder-10.0.1-c486717c322b-windows-x64.msi "https://download.splunk.com/products/universalforwarder/releases/10.0.1/windows/splunkforwarder-10.0.1-c486717c322b-windows-x64.msi"'
-}
+subprocess.run([splunk_path, "add", "monitor", "WinEventLog://Application"])
+subprocess.run([splunk_path, "add", "monitor", "WinEventLog://Systen"])
+subprocess.run([splunk_path, "add", "monitor", "WinEventLog://Security"])
 
-print("Which Machine Are You Using?")
-for i in machines:
-    print("-",i)
-
-machine_type = input()
-for i in machines:
-    if machine_type == i:
-        
+subprocess.run(["net", "stop", "splunkforwarder"])
+subprocess.run(["net", "start", "splunkforwarder"])
