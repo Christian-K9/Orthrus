1bb40a2367bb92d75b6e872353698c34df703fe2
Christian-K9 <164112138+Christian-K9@users.noreply.github.com>
2025-11-20 17:39:12 -0500

    Update forwarders.py

--- forwarders.py
+++ forwarders.py
@@ -1,20 +1,23 @@
 import subprocess
 import getpass
 import sys
+import os
 
 forwarder = "splunkforwarder-10.0.1-c486717c322b-windows-x64.msi"
 url = "https://download.splunk.com/products/universalforwarder/releases/10.0.1/windows/splunkforwarder-10.0.1-c486717c322b-windows-x64.msi"
 
-subprocess.run(["wget", "-O", forwarder, url])
+subprocess.run(["powershell","-Command", f"wget '{url}' -OutFile '{forwarder}'"])
+
 
 splunk_path = r"C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe"
 splunk_server = input("What Is the Splunk Server IP?: ")
 password = getpass.getpass("What is the Splunk Password: ")
 
-subprocess.run([splunk_path, "add", "forward-server", splunk_server])
+subprocess.run([splunk_path, "add", "forward-server", f"{splunk_server}:9997",
+                "-auth", f"admin:{password}"])
 
 subprocess.run([splunk_path, "add", "monitor", "WinEventLog://Application"])
-subprocess.run([splunk_path, "add", "monitor", "WinEventLog://Systen"])
+subprocess.run([splunk_path, "add", "monitor", "WinEventLog://System"])
 subprocess.run([splunk_path, "add", "monitor", "WinEventLog://Security"])
 
 subprocess.run(["net", "stop", "splunkforwarder"])
