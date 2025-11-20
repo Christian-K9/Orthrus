e0fb3604bb3d2051e42a080d5d5cdff80c692a8d
Christian-K9 <164112138+Christian-K9@users.noreply.github.com>
2025-11-20 17:28:21 -0500

    Update forwarders.py

--- forwarders.py
+++ forwarders.py
@@ -1,9 +1,15 @@
 import subprocess
+import getpass
 import sys
+
+forwarder = "splunkforwarder-10.0.1-c486717c322b-windows-x64.msi"
+url = "https://download.splunk.com/products/universalforwarder/releases/10.0.1/windows/splunkforwarder-10.0.1-c486717c322b-windows-x64.msi"
+
+subprocess.run(["wget", "-O", forwarder, url])
 
 splunk_path = r"C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe"
 splunk_server = input("What Is the Splunk Server IP?: ")
-AUTH = "admin:", input("What Is The Admin Password?: ")
+password = getpass.getpass("What is the Splunk Password: ")
 
 subprocess.run([splunk_path, "add", "forward-server", splunk_server])
 
