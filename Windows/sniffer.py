9226c823d773aed551ff2d2dd0db86c467765103
Christian-K9 <164112138+Christian-K9@users.noreply.github.com>
2026-02-18 17:13:45 -0500

    Update sniffer.py

--- Windows/sniffer.py
+++ Windows/sniffer.py
@@ -7,6 +7,8 @@ red = "\033[31m"
 green = "\033[32m"
 yellow = "\033[33m"
 
+snort_location = r"C:\Users\Administrator"
+
 def run(cmd):
     subprocess.run(cmd, check=True)
 
@@ -14,37 +16,43 @@ def run(cmd):
 def install():
 
     #Snort File Location
-    snort_location = r"C:\Users\Administrator\Snort"
+    global snort_location
 
     answer = None
     print(f"{yellow} File Location of Snort: {snort_location}")
     answer = input(f"Change File Location? (y/n) : {reset}")
     if answer == "y":
-        snort_location = input("Enter File Location: ")
+        snort_location = input(r"Enter File Location: ")
+
 
     npcap_location = r"C:\Users\Administrator\npcap.exe"
 
     print(f"{yellow} File Location of npcap: {npcap_location}")
     answer = input(f"Change File Location? (y/n) : {reset}")
     if answer == "y":
-        npcap_location = input("Enter File Location")
+        npcap_location = input(r"Enter File Location")
 
     #Install Snort
     # #Invoke-WebRequest -Uri "https://www.snort.org/downloads/snort/snort-2.9.20.tar.gz" -Headers @{ "User-Agent" = "Mozilla/5.0" } -OutFile "script.tar.gz"
+    print("Creating Snort Folder")
+    snort_file = os.path.join(snort_location, "Snort_Tar")
+    tar_file = os.path.join(snort_file, "snort.tar.gz")
+    print(f"Snort File: {snort_file}")
+    subprocess.run(["mkdir", snort_file], shell=True)
     print(f"{yellow} Installing Snort... {reset}")
-    compressed_file = snort_location + "tar.gz"
+    print(f"{yellow} Outputting Snort to {snort_file}")
     run(["powershell", "-Command", "Invoke-WebRequest", "-Uri", "https://www.snort.org/downloads/snort/snort-2.9.20.tar.gz",
-        "-Headers", "@{ 'User-Agent' = 'Mozilla/5.0' }", "-OutFile", compressed_file])
+        "-Headers", "@{ 'User-Agent' = 'Mozilla/5.0' }", "-OutFile", tar_file])
     print(f"{yellow} Checking If Snort File Exist... {reset}")
-    if os.path.exists(compressed_file):
+    if os.path.exists(snort_file):
         print(f"{green} File Exists {reset}")
     else:
-        print(f"{red} Error: File {snort_location} Does not Exists. Check File Permissions {reset}")
+        print(f"{red} Error: File {snort_file} Does not Exists. Check File Permissions {reset}")
         exit()
     #uncompress snort
     print("Uncompressing Snort File...")
-    subprocess.run(["tar", "-xvzf", compressed_file])
-
+    subprocess.run(["tar", "-xvzf", tar_file, "-C", snort_location])
+    snort_location = os.path.join(snort_location, "snort-2.9.20")
     #Install npcap
     #Invoke-WebRequest -Uri "https://npcap.com/dist/npcap-1.87.exe" -Headers @{ "User-Agent" = "Mozilla/5.0" } -OutFile "script.py"
     run(["powershell", "-Command", "Invoke-WebRequest", "-Uri", "https://npcap.com/dist/npcap-1.87.exe",
@@ -57,22 +65,62 @@ def install():
         print(f"{red} Error: File {snort_location} Does not Exists. Check File Permissions {reset}")
         exit()
 
+def install_alternate():
+
+    #Snort File Location
+    global snort_location
+
+    answer = None
+    print(f"{yellow} File Location of Snort: {snort_location}")
+    answer = input(f"Change File Location? (y/n) : {reset}")
+    if answer == "y":
+        snort_location = input(r"Enter File Location: ")
+
+
+    npcap_location = r"C:\Users\Administrator\npcap.exe"
+
+    print(f"{yellow} File Location of npcap: {npcap_location}")
+    answer = input(f"Change File Location? (y/n) : {reset}")
+    if answer == "y":
+        npcap_location = input(r"Enter File Location")
+
+    #Install Snort
+    # #Invoke-WebRequest -Uri "https://www.snort.org/downloads/snort/snort-2.9.20.tar.gz" -Headers @{ "User-Agent" = "Mozilla/5.0" } -OutFile "script.tar.gz"
+    print("Creating Snort Folder")
+    snort_file = os.path.join(snort_location, "Snort_Tar")
+    tar_file = os.path.join(snort_file, "snort.tar.gz")
+    print(f"Snort File: {snort_file}")
+    subprocess.run(["mkdir", snort_file], shell=True)
+    print(f"{yellow} Installing Snort... {reset}")
+    print(f"{yellow} Outputting Snort to {snort_file}")
+    run(["powershell", "-Command", "Invoke-WebRequest", "-Uri", "https://www.snort.org/downloads/snort/Snort_2_9_20_Installer.x64.exe",
+        "-Headers", "@{ 'User-Agent' = 'Mozilla/5.0' }", "-OutFile", tar_file])
+    print(f"{yellow} Checking If Snort File Exist... {reset}")
+
+def download_rules():
     #Download Snort Rules Files
     #https://rules.emergingthreats.net/open/snort-2.9.0/rules/emerging-dns.rules
-    rules = ["dns", "exploit", "local", "malware", "policy", "web_server"]
+    rules_location = os.path.join(snort_location, "snort-2.9.20", "rules")
+    subprocess.run(["mkdir", rules_location], shell=True)
+    rules = ["dns", "exploit", "malware", "policy", "web_server"]
     for i in rules:
         print(f"Downloading Rule For {i}")
-        rule_url = "https://rules.emergingthreats.net/open/snort-2.9.0/rules"
-        rule_wr = os.path.join(rule_url, i, ".rules")
-        rule_file = os.path.join(snort_location, "etc", "rules", i)
+        rule_url = r"https://rules.emergingthreats.net/open/snort-2.9.20/rules"
+        rule = i + ".rules"
+        rule_wr = f"{rule_url}/emerging-{i}.rules"
+        print(f"Downloading Rule {rule_wr}")
+        rule_file = os.path.join(snort_location, "snort-2.9.20", "rules", f"{i}.rules")
+        print(f"Outputting To {rule_file}")
         run(["powershell", "-Command", "Invoke-WebRequest", "-Uri", rule_wr,
         "-Headers", "@{ 'User-Agent' = 'Mozilla/5.0' }", "-OutFile", rule_file])
 
+def configure():
     #Change Snort Config File
     print("Updating Snort File")
     conf = os.path.join(snort_location, "etc", "snort.conf")
+    print(f"Configuration File: {conf}")
     original = conf
-    temp = 'snort_temp.conf'
+    temp = r'snort_temp.conf'
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
 
@@ -87,10 +135,9 @@ def install():
 
 #activate snort
 def activate():
-    snort_location = r"C:\Users\Administrator"
     #snort -l snort_path -L alerts.log -i <interface>
     interface = 0
-    application = "f{snort_location}\Snort\bin\snort.exe"
+    application = f"{snort_location}\bin\snort.exe"
     result = subprocess.run([application, "-W"], capture_output=True, text=True)
     lines = result.stdout.splitlines()
     for line in lines:
@@ -104,4 +151,4 @@ def activate():
     log_path = os.path.join(snort_location, "log")
     run([application, "-l", log_path, "-L", "alerts.log", "-i", interface])
 
-install()
+install_alternate()
